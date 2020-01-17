62c32ad658f9d5f5e44c3bcffe31e39c12178d13
Yilun-Sun <osirisnero@gmail.com>
2020-01-16 22:07:54 -0500

    Updates

---
+++ precache-manifest.294d850e5cf4fcd7aaf66fc2d33633a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0751da4a3f5f69a2b465b0b65d5ad174",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "2eb5200c6b24558b9969",
+    "url": "/game-of-life/static/css/main.ef8ca55a.chunk.css"
+  },
+  {
+    "revision": "0601e9075462c955d8c7",
+    "url": "/game-of-life/static/js/2.81caa960.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/game-of-life/static/js/2.81caa960.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2eb5200c6b24558b9969",
+    "url": "/game-of-life/static/js/main.9f650f7e.chunk.js"
+  },
+  {
+    "revision": "691a679e02b0fea3427a",
+    "url": "/game-of-life/static/js/runtime-main.c6fc08dc.js"
+  }
+]);
